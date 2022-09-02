fdcb37b42c6bc8f1b033923268f75fab83120f68
paulamilani <paula.milani@arquivei.com.br>
2022-09-02 12:35:02 -0300

    ajuste + demo

--- app/src/main/java/br/edu/listPad/model/ItemLista.kt
+++ app/src/main/java/br/edu/listPad/model/ItemLista.kt
@@ -5,5 +5,5 @@ import java.io.Serializable
 class ItemLista(
     var id_item: Int? = null,
     var nome_item: String = "",
-    var lista_fk: Int = 0
+    var lista_fk: Int? = null
 ) : Serializable
